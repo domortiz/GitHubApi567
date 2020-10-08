92b6c24f9074a61445e28b00db45a35f621b333d
Dom <dom.ortiz1998@gmail.com>
2020-10-07 21:02:22 -0400

    mocking changes

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![build status of master](https://travis-ci.org/domortiz/GitHubApi567.svg?branch=master)](https://travis-ci.org/domortiz/GitHubApi567)
+[![build status of Mock branch](https://travis-ci.org/domortiz/GitHubApi567.svg?branch=HW05a_Mocking)](https://travis-ci.org/domortiz/GitHubApi567)
--- TestGithub.py
+++ TestGithub.py
@@ -1,13 +1,18 @@
 import unittest
+from unittest import mock
 from GetRepoInfo import check_repo
 
 
 class TestRepoInfo(unittest.TestCase):
     def test_no_name(self):
-        self.assertEqual(check_repo(''), False)
+        with mock.patch('GetRepoInfo.check_repo', create=False) as MockCheckRepo:
+            MockCheckRepo.return_value = False
+            self.assertEqual(MockCheckRepo(''), False)
 
     def test_name(self):
-        self.assertEqual(check_repo('domortiz'), True)
+        with mock.patch('GetRepoInfo.check_repo', create=False) as MockCheckRepo:
+            MockCheckRepo.return_value = True
+            self.assertEqual(MockCheckRepo('domortiz'), True)
 
 
 if __name__ == '__main__':
